d9225ab75b8850bf5e7b90b43101a657aef0d5c3
schwitzguebelmar <marc.schwitzguebel_externe@rte-france.com>
2025-12-31 09:43:21 +0100

    Create file exporter class

--- gridcapa-core-valid-day-ahead-conservative-app/src/main/java/com/farao_community/farao/gridcapa_core_valid_day_ahead_conservative/app/request/CoreValidD2ConservativeHandler.java
+++ gridcapa-core-valid-day-ahead-conservative-app/src/main/java/com/farao_community/farao/gridcapa_core_valid_day_ahead_conservative/app/request/CoreValidD2ConservativeHandler.java
@@ -15,51 +15,36 @@ import com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative.api.
 import com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative.api.resource.CoreValidD2ConservativeRequest;
 import com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative.app.services.BranchMaxIvaService;
 import com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative.app.services.CnecRamFilter;
+import com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative.app.services.FileExporter;
 import com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative.app.services.FileImporter;
 import com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative.app.util.ConservativeIvaCalculationUtils;
-import com.farao_community.farao.minio_adapter.starter.MinioAdapter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import org.slf4j.Logger;
 import org.slf4j.MDC;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative.app.util.CoreValidD2Constants.GRIDCAPA_TASK_ID;
-import static com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative.app.util.CoreValidD2Constants.IVA_BRANCH_JSON_FILE_NAME;
-import static com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative.app.util.CoreValidD2Constants.IVA_RESULT_FILE_TYPE;
-import static com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative.app.util.CoreValidD2Constants.MINIO_DESTINATION_PATH_FORMAT;
-import static com.farao_community.farao.gridcapa_core_valid_day_ahead_conservative.app.util.CoreValidD2Constants.PROCESS_NAME;
 
 @Component
 public class CoreValidD2ConservativeHandler {
 
     private final Logger eventsLogger;
     private final FileImporter fileImporter;
-    private final MinioAdapter minioAdapter;
+    private final FileExporter fileExporter;
     private final BranchMaxIvaService branchMaxIvaService;
     private final CoreHubsConfiguration coreHubsConfiguration;
 
-    @Value("${core-valid-day-ahead-conservative-runner.zone-id}")
-    private String zoneId;
-
     public CoreValidD2ConservativeHandler(final FileImporter fileImporter,
-                                          final MinioAdapter minioAdapter,
+                                          final FileExporter fileExporter,
                                           final Logger eventsLogger,
                                           final BranchMaxIvaService branchMaxIvaService,
                                           final CoreHubsConfiguration coreHubsConfiguration) {
         this.fileImporter = fileImporter;
-        this.minioAdapter = minioAdapter;
+        this.fileExporter = fileExporter;
         this.eventsLogger = eventsLogger;
         this.branchMaxIvaService = branchMaxIvaService;
         this.coreHubsConfiguration = coreHubsConfiguration;
@@ -75,7 +60,7 @@ public class CoreValidD2ConservativeHandler {
         final List<IvaBranchData> branches = branchMaxIvaService.computeBranchData(verticesForCalculus, filteredCnecRams, iniParameters);
         ConservativeIvaCalculationUtils.feedConservativeIVAs(branches, iniParameters);
         final byte[] jsonOutput = ivaBranchesToJson(branches);
-        uploadOutputToMinio(jsonOutput, request.getTimestamp());
+        fileExporter.uploadOutputToMinio(jsonOutput, request.getTimestamp());
         return request.getId();
     }
 
@@ -87,15 +72,6 @@ public class CoreValidD2ConservativeHandler {
                 : importedVertices;
     }
 
-    private void uploadOutputToMinio(final byte[] outputFile, final OffsetDateTime timestamp) {
-        try (final InputStream inputStream = new ByteArrayInputStream(outputFile)) {
-            final String minioOutputPath = makeDestinationMinioPath(timestamp) + IVA_BRANCH_JSON_FILE_NAME;
-            minioAdapter.uploadOutputForTimestamp(minioOutputPath, inputStream, PROCESS_NAME, IVA_RESULT_FILE_TYPE, timestamp);
-        } catch (final IOException e) {
-            throw new CoreValidD2ConservativeInternalException("Error during output file upload", e);
-        }
-    }
-
     private byte[] ivaBranchesToJson(final List<IvaBranchData> branches) {
         final ObjectMapper objectMapper = new ObjectMapper();
         final ObjectWriter ow = objectMapper.writer().withDefaultPrettyPrinter();
@@ -104,11 +80,5 @@ public class CoreValidD2ConservativeHandler {
         } catch (final JsonProcessingException e) {
             throw new CoreValidD2ConservativeInternalException("Error creating JSON from IVA branch data", e);
         }
-    }
-
-    private String makeDestinationMinioPath(final OffsetDateTime offsetDateTime) {
-        final ZonedDateTime targetDateTime = offsetDateTime.atZoneSameInstant(ZoneId.of(zoneId));
-        final DateTimeFormatter df = DateTimeFormatter.ofPattern(MINIO_DESTINATION_PATH_FORMAT);
-        return df.format(targetDateTime);
     }
 }
